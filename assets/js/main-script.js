c20caf5b48437435ca48fba9c16b5ed30b494fa7
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2021-08-04 00:25:32 +0100

    first commit

---
+++ assets/js/main-script.js
@@ -0,0 +1,58 @@
+
+
+var btn = $('#button-scroll-top');
+
+
+$(document).ready(function(){
+    
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            $("#navbar").removeClass('navbar-light');
+            $("#navbar").removeClass('navbar-light');
+            $("#navbar").addClass('bg-dark');
+            $("#navbar").addClass('navbar-dark');
+         
+        }
+  
+        else{
+            $("#navbar").removeClass('bg-dark');
+            $("#navbar").removeClass('navbar-dark');
+            $("#navbar").addClass('bg-light');
+            $("#navbar").addClass('navbar-light');
+        }
+    });
+
+$("#navbar-toggler").on('click',function(){
+
+  if($('#navbar-toggler').hasClass('navbar-toggler')){
+    $("#navbar-toggler").removeClass('navbar-toggler');
+    $(".navbar-toggler-icon").css("visibility","hidden");
+    console.log($('#navbar-toggler').hasClass('navbar-toggler'));
+    $("#navbar-toggler").addClass('btn-close');
+  }else if($('#navbar-toggler').hasClass('btn-close')){
+    $("#navbar-toggler").removeClass('btn-close');
+    $(".navbar-toggler-icon").css("visibility","visible");
+    console.log($('#navbar-toggler').hasClass('btn-close'));
+    $("#navbar-toggler").addClass('navbar-toggler');
+  }
+
+});
+
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 200) {
+          btn.addClass('show');
+        } else {
+          btn.removeClass('show');
+        }
+      });
+      
+      btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+      });
+      
+
+
+  });
---
+++ serviceWorker.js
@@ -0,0 +1,35 @@
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/assets/js/main.js",
+  "/assets/js/main-script.js",
+  "/assets/images/hello-icon-128.png",
+  "/assets/images/hello-icon-144.png",
+  "/assets/images/hello-icon-152.png",
+  "/assets/images/hello-icon-192.png",
+  "/assets/images/hello-icon-256.png",
+  "/assets/images/hello-icon-512.png",
+  "/assets/images/logo/logo.jpeg"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+ });
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
